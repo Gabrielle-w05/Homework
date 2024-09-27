504041976b0fe58f47e401c180733ff847ee6c12
Gabrielle-w05 <gabriellewestbrook05@gmail.com>
2024-09-26 23:26:07 -0500

    Pizza Place Homework for week 2

---
+++ week2/pizzaplace.js
@@ -0,0 +1,21 @@
+let pizzaPlace = "Gabby's Pizza";
+let numberOfToppings = 8;
+console.log(pizzaPlace);
+console.log(numberOfToppings);
+typeof pizzaPlace;
+typeof numberOfToppings;
+console.log(typeof pizzaPlace);
+console.log(typeof numberOfToppings);
+const greet =
+  `"Welcome to ${pizzaPlace},` +
+  ` where we have ${numberOfToppings} different toppings for our delicious pizzas!"`;
+console.log(greet);
+if (numberOfToppings < 10) {
+  console.log("Quality, not quantity.");
+} else {
+  numberOfToppings > 10;
+  console.log("A whole lot of pizza.");
+}
+for (let i = 1; i++ <= 8; i++) {
+  console.log(i);
+}
